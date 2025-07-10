f206fa076710a08b071285a37e3f9dd9afcf79b5
Brian Chu <brianmchu42@gmail.com>
2025-07-09 17:50:00 -0700

    add config arguments

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from transformers import AutoModelForSequenceClassification, AutoTokenizer, Trainer, TrainingArguments
 from dataloader import SNPDataset
 from torch.utils.data import random_split
+import wandb
+import os
+os.environ["WANDB_PROJECT"] = "ABCD-caduceus"
+
 
 def main():
     model_name = "kuleshov-group/caduceus-ps_seqlen-131k_d_model-256_n_layer-16"
     tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
     model = AutoModelForSequenceClassification.from_pretrained(model_name, trust_remote_code=True)
     data = SNPDataset(tokenizer)
-    args = TrainingArguments()
+    args = TrainingArguments(
+            output_dir="./caduceus-output",
+            per_device_train_batch_size=4,
+            gradient_accumulation_steps=4,
+            num_train_epochs=10,
+            save_steps=500,
+            report_to="wandb",
+            logging_steps=1,
+            torch_compile=True,
+            torch_compile_backend="inductor",
+            torch_compile_mode="default",
+            )
 
     train, test = random_split(data, [0.8, 0.2])
 
