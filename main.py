381b2d308ce353d8b178c931fff7bd73d5a29a90
Brian Chu <brianmchu42@gmail.com>
2025-07-14 11:20:42 -0700

    config changes for training setup

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from transformers import AutoModelForSequenceClassification, AutoTokenizer, Trainer, TrainingArguments
+from transformers import (AutoModelForSequenceClassification, 
+                          AutoTokenizer, 
+                          Trainer, 
+                          TrainingArguments,
+                          get_cosine_schedule_with_warmup,)
+from bitsandbytes.optim import AdamW
 from dataloader import SNPDataset
 from torch.utils.data import random_split
 import wandb
@@ -11,14 +16,31 @@ def main():
     tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
     model = AutoModelForSequenceClassification.from_pretrained(model_name, trust_remote_code=True)
     data = SNPDataset(tokenizer)
+    
+    '''
+    following training regimen in the paper:
+    - change optimizer and scheduler to cosine annealing
+    - batch size = 2^20 tokens per batch with a sequence length of 16569bp ~= 63
+    '''
+    # optimizer = AdamW(model.parameters(), lr=8e-3, weight_decay=0.01, betas=(0.95, 0.9), )
+    # scheduler = get_cosine_schedule_with_warmup(
+    #         optimizer,
+    #         num_warmup_steps=500,           # about 5-10% of the total training steps
+    #         num_training_steps=int(0.8*len(data)),
+    #         )
     args = TrainingArguments(
             output_dir="./caduceus-output",
-            per_device_train_batch_size=4,
+            run_name="nikola-single-gpu",
+            per_device_train_batch_size=4, # make it a nice round power of 2
             gradient_accumulation_steps=4,
-            num_train_epochs=10,
+            num_train_epochs=2,
             save_steps=500,
+            save_safetensors=False,         # model is setup such that safetensors can't save properly
+            bf16=True,
             report_to="wandb",
             logging_steps=1,
+            dataloader_num_workers=4,
+            dataloader_pin_memory=True,
             torch_compile=True,
             torch_compile_backend="inductor",
             torch_compile_mode="default",
@@ -29,7 +51,9 @@ def main():
     trainer = Trainer(model,
                       args=args,
                       train_dataset = train, 
-                      eval_dataset = test)
+                      eval_dataset = test,
+    #                  optimizers=(optimizer, scheduler)
+                      )
     trainer.train()
     
 
