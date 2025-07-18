31ca6dd06003fb28dff96cfcba6a059f03b8efec
Brian Chu <brianmchu42@gmail.com>
2025-07-18 16:52:22 -0700

    add optimizer and scheduler

--- main.py
+++ main.py
@@ -24,21 +24,21 @@ def main():
     - change optimizer and scheduler to cosine annealing
     - batch size = 2^20 tokens per batch with a sequence length of 16569bp ~= 63
     '''
-    # optimizer = AdamW(model.parameters(), lr=8e-3, weight_decay=0.01, betas=(0.95, 0.9), )
-    # scheduler = get_cosine_schedule_with_warmup(
-    #         optimizer,
-    #         num_warmup_steps=100,           # about 5-10% of the total training steps
-    #         num_training_steps=1166
-    #         )
+    optimizer = AdamW(model.parameters(), lr=8e-3, weight_decay=0.01, betas=(0.95, 0.9), )
+    scheduler = get_cosine_schedule_with_warmup(
+            optimizer,
+            num_warmup_steps=100,           # about 5-10% of the total training steps
+            num_training_steps=1166
+            )
     args = TrainingArguments(
             output_dir="./caduceus-output",
-            run_name="nikola-single-gpu",
+            run_name="nikola-single-gpu+AdamW+Cosine",
             per_device_train_batch_size=4, # make it a nice round power of 2
             gradient_accumulation_steps=4,
             num_train_epochs=2,
             save_steps=500,
             save_safetensors=False,         # model is setup such that safetensors can't save properly
-            bf16=True,                    # no mixed precision for now
+            bf16=True,
             max_grad_norm=1.0,              # gradient clipping to prevent NaN
             report_to="wandb",
             logging_steps=1,
@@ -55,7 +55,7 @@ def main():
                       args=args,
                       train_dataset = train, 
                       eval_dataset = test,
-    #                   optimizers=(optimizer, scheduler)
+                      optimizers=(optimizer, scheduler)
                       )
     trainer.train()
     
